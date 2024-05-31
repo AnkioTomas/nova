2725188d670baf291abfb03243ad68d829306d08
Ankio <ankio@ankio.com>
2024-05-31 16:55:50 +0800

    :zap: 测试优化

--- tests/route.tests.php
+++ tests/route.tests.php
@@ -4,16 +4,7 @@ use nova\framework\request\Route;
 use function nova\framework\route;
 
 include "testHeader.php";
-function check($obj1, $obj2)
-{
-    foreach (get_object_vars($obj1) as $key => $value) {
-        try {
-            assert($obj1->$key == $obj2->$key);
-        }catch (AssertionError $e){
-            echo "Error: obj1->$key: ".print_r($value,true)." != obj2->$key: " . print_r($obj2->$key ,true). "\n";
-        }
-    }
-}
+
 function testRoute(): void
 {
     $arrays = [
@@ -27,13 +18,13 @@ function testRoute(): void
     foreach ($arrays as $key => $value) {
         Route::add($key, $value);
     }
-    check(Route::dispatch("", "GET"), route("index", "main", "index"));
-    check(Route::dispatch("/", "GET") , route("index", "main", "index"));
-    check(Route::dispatch("/index/test-1-id.html", "GET"), route("index", "main", "user", ["user" => 1]));
-    check(Route::dispatch("/index/test-1.html", "GET") , route("index", "main", "test", ["id" => 1]));
-    check(Route::dispatch("/index/hi/1.tpl", "GET"),route("index", "hi", "number", ["hi" => "hi", "number" => 1]));
-    check(Route::dispatch("/index/test/module/", "GET"), route("module", "main", "index"));
-    check(Route::dispatch("/index/main/module", "GET"), route("index", "main", "module"));
+    checkObject(Route::dispatch("", "GET"), route("index", "main", "index"));
+    checkObject(Route::dispatch("/", "GET") , route("index", "main", "index"));
+    checkObject(Route::dispatch("/index/test-1-id.html", "GET"), route("index", "main", "user", ["user" => 1]));
+    checkObject(Route::dispatch("/index/test-1.html", "GET") , route("index", "main", "test", ["id" => 1]));
+    checkObject(Route::dispatch("/index/hi/1.tpl", "GET"),route("index", "hi", "number", ["hi" => "hi", "number" => 1]));
+    checkObject(Route::dispatch("/index/test/module/", "GET"), route("module", "main", "index"));
+    checkObject(Route::dispatch("/index/main/module", "GET"), route("index", "main", "module"));
     echo "All tests passed\n";
 }
 
--- tests/testHeader.php
+++ tests/testHeader.php
@@ -4,3 +4,13 @@ $GLOBALS['__nova_app_start__'] = microtime(true);
 $GLOBALS['__nova_app_config__'] =  include "../src/config.php";
 $GLOBALS['__nova_session_id__'] = uniqid('session_', true);
 include "../src/nova/framework/constants.php";
+function checkObject($obj1, $obj2)
+{
+    foreach (get_object_vars($obj1) as $key => $value) {
+        try {
+            assert($obj1->$key == $obj2->$key);
+        }catch (AssertionError $e){
+            echo "Error: obj1->$key: ".print_r($value,true)." != obj2->$key: " . print_r($obj2->$key ,true). "\n";
+        }
+    }
+}
